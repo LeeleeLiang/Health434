0b7301f5fb07a314c81fded0dcfd45213d6e1390
joscho11 <57696175+joscho11@users.noreply.github.com>
2021-04-05 18:38:18 -0400

    updated calc

--- DailyUp/app/src/main/java/com/example/dailyup/BMICalculator.java
+++ DailyUp/app/src/main/java/com/example/dailyup/BMICalculator.java
@@ -29,26 +29,33 @@ public class BMICalculator extends AppCompatActivity {
 
         String str = "";
         if(Float.compare(bmi, 15f) <= 0) {
-            str = bmi + "\nCRITICALLY UNDERWEIGHT";
+            str = "BMI: " + bmi + "\nCRITICALLY UNDERWEIGHT\n\nRecommendations:\n-Do physical activity for 30 minutes 3 times a week."
+            + "\n-Each nutritious meals 3 times a day.\n-See a doctor if lost more than 10 pounds of your body weight in the last year or less";
         }
         else if(Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 19f) <= 0) {
-            str = bmi + "\nUNDERWEIGHT";
+            str = "BMI: " + bmi + "\nUNDERWEIGHT\n\nRecommendations:\n-Eat more frequently\nTry nutrient-rich smoothies"
+                    + "\n-exercise a few times a week.";
         }
         else if(Float.compare(bmi, 19f) > 0 && Float.compare(bmi, 24f) <= 0) {
-            str = bmi + "\nNORMAL";
+            str = "BMI: " + bmi + "\nNORMAL\n\nRecommendations:\n-Keep doing your normal routine, you're Healthy!";
         }
         else if(Float.compare(bmi, 24f) > 0 &&Float.compare(bmi, 29f) <= 0) {
-            str = bmi + "\nOVERWEIGHT";
+            str = "BMI: " + bmi + "\nOVERWEIGHT\n\nRecommendations:\n-Do physical activity for 30 minutes 2 times a week."
+            + "\n-Eat less junk food.";
         }
         else if(Float.compare(bmi, 29f) > 0 && Float.compare(bmi, 34f) <= 0) {
-            str = bmi + "\nOBESE";
+            str = "BMI: " + bmi + "\nOBESE\n\nRecommendations:\n-Do physical activity for 30 minutes 4 times a week."
+                    + "\n-Only eat meat every other day.";
         }
         else if(Float.compare(bmi, 34f) > 0) {
-            str = bmi + "\nCRITICALLY OBESE";
+            str = "BMI: " + bmi + "\nCRITICALLY OBESE\n\nRecommendations:\n-Seek medical advice.\n" +
+            "-Eat healthier.\n-Take a walk a few times a week.";
         }
         else {
             str = "BMI unable to be calculated";
         }
+
+        str += "\n\nUse our workout planner for more advice on how to get in better shape!";
         return str;
     }
 
